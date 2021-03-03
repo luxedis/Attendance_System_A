ed75a09ec11a46199465a57c8ab2c3635cb94fec
luxedis <shinobu85@gmail.com>
2021-03-03 12:59:04 +0000

    views/hubs/index編集途中

--- app/controllers/hubs_controller.rb
+++ app/controllers/hubs_controller.rb
@@ -1,7 +1,24 @@
 class HubsController < ApplicationController
+  before_action :set_hub
+  
   def index
   end
 
   def show
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
   end
 end
