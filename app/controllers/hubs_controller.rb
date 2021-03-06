28d7d17d013e00dba71161014dfa93e20bcf510c
luxedis <shinobu85@gmail.com>
2021-03-06 12:52:01 +0000

    hubs_controller追加

--- app/controllers/hubs_controller.rb
+++ app/controllers/hubs_controller.rb
@@ -1,24 +1,54 @@
 class HubsController < ApplicationController
-  before_action :set_hub
+  before_action :set_hub, only: [:show, :edit, :update, :destroy]
+  before_action :admin_user
   
   def index
+    @hub = Hub.new
   end
 
   def show
   end
   
   def new
+    @hub = Hub.new
   end
   
   def create
+    @hub = Hub.new(hub_params)
+    if @hub.save
+      flash[:success] = "拠点情報を作成しました。"
+      redirect_to hubs_url
+    else
+      ######
+    end
   end
   
   def edit
   end
   
   def update
+    if @hub.update_attributes(hub_params)
+      flash[:success] = "拠点情報を更新しました。"
+      redirect_to hubs_url
+    else
+      ######
+    end
   end
   
   def destroy
+    @hub.destroy
+    flash[:success] = "#{@hub.number}のデータを削除しました。"
+    redirect_to hubs_url
+  end
+  
+  
+  private
+  
+  def set_hub
+    @hub = Hub.find(params[:id])
+  end
+  
+  def hub_params
+    params.require(:hub).permit(:hub_nimber, :name, :attendance)
   end
 end
